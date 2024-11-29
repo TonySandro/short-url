319dd34a5a4f6249aced4e87f027bf57d1514f77
Tony Sandro <85088970+TonySandro@users.noreply.github.com>
2024-11-29 18:54:23 -0300

    feat: ensure SignupController returns 200 if success

--- src/presentation/controller/signup/signup-controller.spec.ts
+++ src/presentation/controller/signup/signup-controller.spec.ts
@@ -1,4 +1,5 @@
-import { success } from "../../helpers/http/http-helper";
+import { MissingParamError } from "../../errors/missing-param-error";
+import { badRequest, success } from "../../helpers/http/http-helper";
 import { SignupController } from "./signup-controller";
 
 const makeSut = () => {
@@ -17,5 +18,29 @@ describe("Signup Controller", () => {
 
     const httpResponse = await sut.handle(httpRequest);
     expect(httpResponse).toEqual(success(httpRequest));
+  });
+
+  test("Should return 400 if no name is provide", async () => {
+    const sut = makeSut();
+    const httpRequest = {
+      body: {
+        password: "any_password",
+      },
+    };
+
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse).toEqual(badRequest(new MissingParamError("name")));
+  });
+
+  test("Should return 400 if no password is provide", async () => {
+    const sut = makeSut();
+    const httpRequest = {
+      body: {
+        name: "any_name",
+      },
+    };
+
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse).toEqual(badRequest(new MissingParamError("password")));
   });
 });
--- src/presentation/controller/signup/signup-controller.ts
+++ src/presentation/controller/signup/signup-controller.ts
@@ -1,8 +1,17 @@
-import { success } from "../../helpers/http/http-helper";
+import { MissingParamError } from "../../errors/missing-param-error";
+import { badRequest, success } from "../../helpers/http/http-helper";
 import { Controller, HttpRequest, HttpResponse } from "../../protocols";
 
 export class SignupController implements Controller {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    const requiredFields = ["name", "password"];
+
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field));
+      }
+    }
+
     return success(httpRequest);
   }
 }
