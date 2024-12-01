8df33f2ea3113bb9dc6910dbaadd4503d681b95d
Tony Sandro <85088970+TonySandro@users.noreply.github.com>
2024-11-30 22:26:41 -0300

    feat: add sqlite helper

--- src/domain/models/user.ts
+++ src/domain/models/user.ts
@@ -1,5 +1,13 @@
-export interface UserModel {
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("users")
+export class UserModel {
+  @PrimaryGeneratedColumn("uuid")
   id: string;
+
+  @Column({ type: "varchar", length: 255 })
   name: string;
+
+  @Column({ type: "varchar", length: 255, unique: true })
   email: string;
 }
