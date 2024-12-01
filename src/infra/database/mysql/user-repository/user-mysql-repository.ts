3e5879be3ec085dbc0d8e5e70ef4421476748933
Tony Sandro <85088970+TonySandro@users.noreply.github.com>
2024-12-01 18:20:10 -0300

    feat: ensure UserMysqlRepository add user

--- src/infra/database/mysql/user-repository/user-mysql-repository.ts
+++ src/infra/database/mysql/user-repository/user-mysql-repository.ts
@@ -1,7 +1,10 @@
 import { AddUserModel } from "../../../../domain/usecases/add-user";
+import { MysqlHelper } from "../helper/mysql-helper";
 
 export class UserMysqlRepository {
   async add(user: AddUserModel): Promise<AddUserModel> {
-    return user;
+    const result = await MysqlHelper.add(user);
+
+    if (result) return user;
   }
 }
