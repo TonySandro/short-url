29e90f0caee3ae6c9143b932c9259c1c31165bf5
Tony Sandro <85088970+TonySandro@users.noreply.github.com>
2024-12-02 09:30:19 -0300

    feat: ensure UrlShortenerController return 400 if NodeUrlShortener returns an error

--- src/presentation/controller/url-shortener/url-shortener-controller.spec.ts
+++ src/presentation/controller/url-shortener/url-shortener-controller.spec.ts
@@ -1,15 +1,56 @@
 import { MissingParamError } from "../../errors";
+import { badRequest } from "../../helpers/http/http-helper";
+import { NodeUrlShortener } from "../../helpers/shortener/node-url-shortener";
+import { HttpRequest } from "../../protocols";
 import { UrlShortenerController } from "./url-shortener-controller";
 
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    originalUrl: "https://teddydigital.io/sobre/",
+  },
+});
+
+const makeNodeUrlShortener = (): NodeUrlShortener => {
+  class NodeUrlShortenerStub implements NodeUrlShortener {
+    short(originalUrl: string) {
+      return {
+        originalUrl: "https://teddydigital.io/sobre/",
+        shortUrl: "http://localhost/aZbKq7",
+      };
+    }
+  }
+
+  return new NodeUrlShortenerStub();
+};
+
 const makeSut = () => {
-  return new UrlShortenerController();
+  const nodeUrlShortenerStub = makeNodeUrlShortener();
+  const sut = new UrlShortenerController(nodeUrlShortenerStub);
+
+  return {
+    sut,
+    nodeUrlShortenerStub,
+  };
 };
 
 describe("Url Shortener Controller", () => {
-  test("Should return 400 if no url is provided ", async () => {
-    const sut = makeSut();
+  test("Should return 400 if no originalUrl is provided ", async () => {
+    const { sut } = makeSut();
     const httpResponse = await sut.handle({ body: {} });
 
-    expect(httpResponse.body).toEqual(new MissingParamError("url"));
+    expect(httpResponse.body).toEqual(new MissingParamError("originalUrl"));
+  });
+
+  test("Should return 400 if NodeUrlShortener returns an error", async () => {
+    const { sut, nodeUrlShortenerStub } = makeSut();
+    jest
+      .spyOn(nodeUrlShortenerStub, "short")
+      .mockReturnValueOnce(new MissingParamError("any_field"));
+
+    const httpResponse = await sut.handle(makeFakeRequest());
+
+    expect(httpResponse).toEqual(
+      badRequest(new MissingParamError("any_field"))
+    );
   });
 });
--- src/presentation/controller/url-shortener/url-shortener-controller.ts
+++ src/presentation/controller/url-shortener/url-shortener-controller.ts
@@ -1,13 +1,21 @@
 import { MissingParamError } from "../../errors";
 import { badRequest } from "../../helpers/http/http-helper";
+import { NodeUrlShortener } from "../../helpers/shortener/node-url-shortener";
 import { Controller, HttpRequest, HttpResponse } from "../../protocols";
 
 export class UrlShortenerController implements Controller {
+  constructor(private readonly nodeUrlShortener: NodeUrlShortener) {}
+
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    if (!httpRequest.body.url) {
-      return badRequest(new MissingParamError("url"));
+    if (!httpRequest.body.originalUrl) {
+      return badRequest(new MissingParamError("originalUrl"));
     }
 
-    return new Promise(null);
+    const error = this.nodeUrlShortener.short(httpRequest.body.originalUrl);
+    if (error) {
+      return badRequest(error);
+    }
+
+    return new Promise((resolve) => resolve(null));
   }
 }
